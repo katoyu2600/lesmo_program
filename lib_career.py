4a079dd793a0ca115fa62bb5116896477506da6f
katoyu <katoyu4055@gmail.com>
2022-10-24 17:03:59 +0900

    パラシュート分離確認プログラム

--- lib_career.py
+++ lib_career.py
@@ -1,13 +1,21 @@
+#coding: utf-8
+#パラシュート分離機構（ニクロム線）動作確認用プログラム
+#対応ハード：ニクロム線
+
+#モジュールインポート
 import pigpio
 import time 
 
+#電熱線ピン番号
 CAREER_CUT = 6
-
+#インスタンス設定
 pi = pigpio.pi()
 
+#電熱線のピンをOFFにする
 pi.set_mode(CAREER_CUT, pigpio.OUTPUT)
 pi.write(CAREER_CUT , 0)
 
+#メイン処理
 def career_cat(c_time) :
     print("点火")
     pi.write(CAREER_CUT , 1 )
